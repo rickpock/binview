c0653d6966f3b7f0e607ef19a1fc0121aeaa39f1
Rick Pocklington <rpocklington@vmware.com>
2021-05-29 17:34:05 -0400

    Parameterize filename for binary viewer. Switch to unsigned char.

--- test/main.c
+++ test/main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 //#include <stdlib.h>
 
-char toPrintableChar(char ch);
-int read16(FILE *fp, char* buffer);
-void print16(char* buffer, int bufferSz);
+unsigned char toPrintableChar(unsigned char ch);
+int read16(FILE *fp, unsigned char* buffer);
+void print16(unsigned char* buffer, int bufferSz);
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        perror("Missing an argument.");
+        return 2;
+    }
+
     FILE *fp;
 
-    fp = fopen("test/resources/example", "r");
+    fp = fopen(argv[1], "r");
 
     if (fp == NULL)
     {
-        perror("Uh-oh");
+        perror("Unable to read file.");
         return 1;
     }
 
     const int BUFFER_SIZE = 16;
-    char buffer[BUFFER_SIZE];
+    unsigned char buffer[BUFFER_SIZE];
     int bytesRead = BUFFER_SIZE;
     while ((bytesRead = read16(fp, buffer)) == BUFFER_SIZE)
     {
@@ -30,12 +36,12 @@ int main()
     return 0;
 }
 
-inline char toPrintableChar(char ch)
+inline unsigned char toPrintableChar(unsigned char ch)
 {
     if (ch < 0x20)
     { return '.'; }
 
-    if (ch == 0x3F)
+    if (ch >= 0x7F)
     { return '.'; }
 
     return ch;
@@ -46,12 +52,12 @@ inline char toPrintableChar(char ch)
  * Buffer must be at least 16 bytes in size
  * Returns the number of bytes written to buffer
  */
-inline int read16(FILE *fp, char* buffer)
+inline int read16(FILE *fp, unsigned char* buffer)
 {
     for (int counter = 0; counter < 16; counter++)
     {
         buffer[counter] = fgetc(fp);
-        if (buffer[counter] == EOF)
+        if (buffer[counter] == (unsigned char)EOF)
         {
             return counter;
         }
@@ -60,7 +66,7 @@ inline int read16(FILE *fp, char* buffer)
     return 16;
 }
 
-inline void print16(char* buffer, int bufferSz)
+inline void print16(unsigned char* buffer, int bufferSz)
 {
     for (int counter = 0; counter < 8; counter++)
     {
